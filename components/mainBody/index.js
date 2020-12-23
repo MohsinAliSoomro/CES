b6905b0d30e67791dbf4d2d6d82eb0c50d011d2f
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2020-12-23 13:27:56 +0500

    evergreen dialog configure

--- components/mainBody/index.js
+++ components/mainBody/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
-
+import React from 'react';
+import { Pane, Dialog, Button } from 'evergreen-ui';
 function index() {
+	const [ value, setValue ] = React.useState({ isShow: false });
 	return (
 		<div
 			style={{
@@ -8,13 +10,31 @@ function index() {
 				marginTop: '10px',
 				textAlign: 'center',
 				backgroundColor: 'linear-gradient(to right, #abbaab, #ffffff)',
-                borderRadius: '10px',
-                
+				borderRadius: '10px'
 			}}
 		>
 			<Image src="/illustration/std.png" alt="std" width="500" height="500" />
+
+			<DialogComponent value={value} setValue={setValue} />
 		</div>
 	);
 }
 
 export default index;
+
+const DialogComponent = ({value,setValue}) => {
+	return (
+		<Pane>
+			<Dialog
+				isShown={value.isShown}
+				title="Dialog title"
+				onCloseComplete={() => setValue({ isShown: false })}
+				confirmLabel="Custom Label"
+			>
+				Dialog content
+			</Dialog>
+
+			<Button onClick={() => setValue({ isShown: true })}>Show Dialog</Button>
+		</Pane>
+	);
+};
