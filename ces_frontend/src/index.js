f86441a6eefcc11e4dd0044209158a65a3a0f30f
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2020-12-23 18:10:41 +0500

    install react-router-dom dotenv and antd and structure the project

--- ces_frontend/src/index.js
+++ ces_frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'antd/dist/antd.css';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
