6fd4432d0c243fef65028eab4a099cf0e0f464ac
tabarak5 <tacitim5@gmail.com>
2021-01-15 00:44:13 +0500

    Error fixed

--- API/models/dept.model.js
+++ API/models/dept.model.js
@@ -1,19 +1,20 @@
-module.exports = mongoose => {
-    var schema = mongoose.Schema(
-      {
-        title: String,
-        description: String,
-        statusflag: Boolean,
-      },
-      { timestamps: true }
-    );
-  
-    schema.method("toJSON", function() {
-      const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
-      return object;
-    });
-  
-    const Dept = mongoose.model("dept", schema);
-    return Dept;
-  };
+
+module.exports = (mongoose) => {
+  var schema = mongoose.Schema(
+    {
+      title: String,
+      description: String,
+      statusflag: Boolean,
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Dept = mongoose.model("dept", schema);
+  return Dept;
+};
