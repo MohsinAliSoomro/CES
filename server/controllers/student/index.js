5cd30f1afbc175af86f90a9f2cff4ea6b39b9a7c
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-02-20 15:18:57 +0500

    student controller and model created

--- server/controllers/student/index.js
+++ server/controllers/student/index.js
@@ -2,10 +2,48 @@ const Student = require('../../models/student');
 
 exports.student = {
 	createStudent: async (req, res) => {
-		const student = await Student(res.body);
-		await student.save().then(res => {
-			res.send(student);
-		}).catch(er=>res.send(er));
-		
+		const student = await Student(req.body);
+		await student
+			.save()
+			.then((re) => {
+				res.send(student);
+			})
+			.catch((er) => res.send(er));
+	},
+	students: async (req, res) => {
+		const student = await Student.find({});
+		res.send(student);
+	},
+	student: async (req, res) => {
+		const student = await Student.find({ _id: req.params.id });
+		res.send(student);
+	},
+	studentDelete: async (req, res) => {
+		const student = await Student.findOneAndDelete({ _id: req.params.id });
+		res.send(student);
+	},
+	studentUpdate: async (req, res) => {
+		const student = await Student.findOne({ _id: req.params.id });
+		if (student) {
+			const std = await Student.findByIdAndUpdate(
+				{ _id: req.params.id },
+				{
+					$set: {
+						firstName: req.body.firstName,
+						lastName: req.body.lastName,
+						fatherName: req.body.fatherName,
+						religion: req.body.religion,
+						surname: req.body.surname,
+						nationality: req.body.nationality,
+						address: req.body.address,
+						district: req.body.district,
+						programId: req.body.programId
+					}
+				}
+			);
+			res.send(std);
+		} else {
+			res.json({ message: 'Error to update student' });
+		}
 	}
 };
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,7 +1,6 @@
-
-
 const router = require('express').Router();
-const {student}=require('../controllers/student')
-router.post('/create', student.createStudent)
+const { student } = require('../controllers/student');
+
+router.post('/create', student.createStudent);
 
 module.exports = router;
