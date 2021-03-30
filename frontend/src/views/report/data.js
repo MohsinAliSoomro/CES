21fa1fe1735e40ae2ac0ccd4cc47fe9a5aa86bbb
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-03-30 20:26:35 +0500

    report props pass

--- frontend/src/views/report/data.js
+++ frontend/src/views/report/data.js
@@ -1,29 +1,260 @@
-import React, { Component} from 'react';
+import React, { Component,useEffect,useState} from 'react';
 import styles from './style.scss';
 import data from './data.json';
 import QRCode from 'qrcode.react';
-export class ComponentToPrint extends Component {
-	constructor(props) {
-		super(props);
-		console.log(this.props)
-		this.state={
-			result: []
-		}
-	}
+// export class ComponentToPrint extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		console.log(this.props)
+// 		this.state={
+// 			result: []
+// 		}
+// 	}
 	
-	componentDidMount() {
-console.log(this.props)
-		fetch('http://localhost:4000/marks/marksStudent/6041241912ad281dace9e4e3')
-			.then((js) => js.json())
-			.then((res) => {
-				this.setState({result:res})
-				 console.log(res)
-			});
-	}
-	render() {
-console.log("Result",this.props)
+// 	componentDidMount() {
+// console.log(this.props)
+// 		fetch('http://localhost:4000/marks/marksStudent/6041241912ad281dace9e4e3')
+// 			.then((js) => js.json())
+// 			.then((res) => {
+// 				this.setState({result:res})
+// 				 console.log(res)
+// 			});
+// 	}
+// 	render() {
+
 		
-		let totSub = 0;
+// 		let totSub = 0;
+// 		let gp = 0;
+// 		let gpa = 0;
+// 		let qrcode = ` Roll No : ${data.rollno} \n Name :  ${data.studentName}  \n Programme : ${data.program} \n Semester : ${data.semester}  \n Session: ${data.session}  \n Exam Type : ${data.papertype}  `;
+// 		function calcgp(obt, tot) {
+// 			let obtper = obt * 100 / tot;
+// 			if (obtper >= 87) {
+// 				return 4.0;
+// 			} else if (obtper >= 85.5) {
+// 				return 3.9;
+// 			} else if (obtper >= 84) {
+// 				return 3.8;
+// 			} else if (obtper >= 82.5) {
+// 				return 3.7;
+// 			} else if (obtper >= 81) {
+// 				return 3.6;
+// 			} else if (obtper >= 79.5) {
+// 				return 3.5;
+// 			} else if (obtper >= 78) {
+// 				return 3.4;
+// 			} else if (obtper >= 76.5) {
+// 				return 3.3;
+// 			} else if (obtper >= 75) {
+// 				return 3.2;
+// 			} else if (obtper >= 73.5) {
+// 				return 3.1;
+// 			} else if (obtper >= 72) {
+// 				return 3.0;
+// 			} else if (obtper >= 70.5) {
+// 				return 2.87;
+// 			} else if (obtper >= 69) {
+// 				return 2.75;
+// 			} else if (obtper >= 67.5) {
+// 				return 2.62;
+// 			} else if (obtper >= 66) {
+// 				return 2.5;
+// 			} else if (obtper >= 64.5) {
+// 				return 2.37;
+// 			} else if (obtper >= 63) {
+// 				return 2.25;
+// 			} else if (obtper >= 61.5) {
+// 				return 2.12;
+// 			} else if (obtper >= 60) {
+// 				return 2.0;
+// 			} else {
+// 				return 0.0;
+// 			}
+// 		}
+// 		function grade(obt, tot) {
+// 			var obtper = obt * 100 / tot;
+// 			if (obtper > 86) {
+// 				return 'A';
+// 			} else if (obtper > 71) {
+// 				return 'B';
+// 			} else if (obtper > 59) {
+// 				return 'C';
+// 			} else {
+// 				return 'Fail';
+// 			}
+// 		}
+// 		if (!result) {
+// 			return <div>Loading....</div>;
+// 		}
+// 		return (
+// 			<div className={styles.container}>
+// 			<div style={{ position: 'relative' }}>
+// 				<div style={{ position: 'absolute', top: '50%', left: '90%' }}>
+// 					<h1>Absolute</h1>
+// 				</div>
+// 				{/* Term back formula and grading formula */}
+// 				<div style={{ display: 'flex' }}>
+// 					<div style={{ border: '1px solid black', marginRight: '70px' }}>
+// 						<p
+// 							style={{
+// 								textAlign: 'center',
+// 								backgroundColor: '#c7c7c7',
+// 									border: '1px solid black',
+// 									fontSize: "11px",
+// 								fontWeight:"bold"
+// 							}}
+// 						>
+// 							Term back formula
+// 					</p>
+// 						<p style={{ padding: '2px', fontSize: "11px" }}>
+// 							A student who is fail in four courses in any semester or did not maintain G.P.A. 1.33 in a
+// 							semester or did not maintain continuity in appearing in tde Examinations but avails semester
+// 							break he/she shall be placed on term back from tde rolls of tde department.
+// 					</p>
+// 					</div>
+// 					<div>
+// 						<table>
+// 							<tbody>
+// 								<tr>
+// 									<td
+// 										colSpan="3"
+// 										className="tdata"
+// 										style={{ textAlign: 'center', backgroundColor: '#c7c7c7' }}
+// 									>
+// 										Grading Formula
+// 								</td>
+// 								</tr>
+// 								<tr>
+// 									<td className="tdata">87-100</td>
+// 									<td className="tdata">A</td>
+// 									<td className="tdata">GP 4.00</td>
+// 								</tr>
+// 								<tr>
+// 									<td className="tdata">72-86</td>
+// 									<td className="tdata">B</td>
+// 									<td className="tdata">GP 3.00 to 3.90</td>
+// 								</tr>
+// 								<tr>
+// 									<td className="tdata">60-71</td>
+// 									<td className="tdata">C</td>
+// 									<td className="tdata">GP 2.00 to 2.90</td>
+// 								</tr>
+// 								<tr>
+// 									<td style={{ textAlign: 'center' }} colSpan="3" className="tdata">
+// 										Below 60 --- Fail
+// 								</td>
+// 								</tr>
+// 							</tbody>
+// 						</table>
+// 					</div>
+// 				</div>
+// 				{/* Heading of department and name*/}
+// 				<div>
+// 					<h1
+// 						style={{
+// 							textAlign: 'center',
+// 							fontWeight: 'bolder',
+// 							fontSize: '30px'
+// 						}}
+// 					>
+// 						{/* {data.ptype} Studies ({data.department}) {result[0].studentId.program} ( */}
+// 					{/* {data.pduration} Years) */}
+// 				</h1>
+// 					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+// 						<p>Department of {data.department}</p>
+// 						<p>Session {data.session}</p>
+// 					</div>
+// 					{/* Name  */}
+// 					<p>
+// 						{data.gender === 0 ? <span>Mr.</span> : <span>Miss.</span>}
+// 						{result.length > 0 && result[0].studentId.firstName}{' '}
+// 						{result.length > 0 && result[0].studentId.lastName}
+// 					</p>
+// 				</div>
+// 				<table>
+// 					<tbody>
+// 						<tr>
+// 							<td>
+// 								{data.semester} semester {data.session} {data.papertype}
+// 							</td>
+// 							<td>Credit hours</td>
+// 							<td>Obt</td>
+// 							<td>Grade</td>
+// 							<td>GP</td>
+// 							<td>Remarks</td>
+// 						</tr>
+// 						{/* <tr style={{padding:"100px"}}>
+// 							{data.subjects.map((m) => {
+// 								return (
+// 									<td>
+// 										{m.name}({m.type})
+// 									</td>
+// 								);
+// 							})}
+// 							<td />
+// 						</tr> */}
+// 						{result.map((sb) => {
+// 							return (
+// 								<tr> 
+// 									 <td>{sb.subjectId.name}</td>
+// 									<td>{sb.subjectId.creditHour}</td>
+// 									<td>{sb.marks[0]}</td> 
+// 									 <td>
+// 										{sb.subjectId.type === 'Theory' ? grade(sb.marks[0], 100) : grade(sb.marks[0], 50)}
+// 									</td>
+// 									<td>
+// 										{sb.subjectId.type === 'Theory' ? (
+// 											calcgp(sb.marks[0], 100)
+// 										) : (
+// 												calcgp(sb.marks[0], 50)
+// 											)}{' '}
+// 									</td>
+// 									<td style={{ display: 'none' }}>
+// 										{(totSub++, (gp += sb.subjectId.type == 'Theory' ? calcgp(sb.marks[0], 100) : calcgp(sb.marks[0], 50)))}
+// 									</td> 
+// 								 </tr>
+// 							);
+// 						})}
+// 						<tr> 
+// 							<td><b>GPA</b></td> <td>{(gpa = gp / totSub).toFixed(2)}</td>
+// 							<td style={{ display: 'none' }}>{(qrcode += `\n GPA : ${gpa.toFixed(2)} `)}</td>
+// 						</tr>
+// 						{/* {data.subjects.map((m) => (
+// 						<tr>
+// 							{' '}
+// 							<td>
+// 								{m.name}({m.type})
+// 							</td>{' '}
+// 							<td>{m.credithr}</td> <td>{m.marks}</td>{' '}
+// 							<td>{m.type === 'TH' ? grade(m.marks, 100) : grade(m.marks, 50)}</td>
+// 							<td>{m.type === 'TH' ? calcgp(m.marks, 100) : calcgp(m.marks, 50)} </td>
+// 							<td style={{ display: 'none' }}>
+// 								{(totSub++, (gp += m.type == 'TH' ? calcgp(m.marks, 100) : calcgp(m.marks, 50)))}
+// 							</td>
+// 							{}
+// 						</tr>
+// 					))} */}
+// 					</tbody>
+// 				</table>
+// 				<QRCode value={qrcode} />
+// 			</div>
+// 			</div>
+// 		);
+// 	};
+// }
+export default function ComponentToPrint({name}) {
+    const [result,setResult]=useState()
+    useEffect(() => {
+        fetch('http://localhost:4000/marks/marksStudent/6041241912ad281dace9e4e3')
+			.then((js) => js.json())
+            .then((res) => {
+                setResult(res)
+			// this.setState({result:res})
+			 console.log(res)
+		});
+    }, [])
+    console.log("name",name)
+    let totSub = 0;
 		let gp = 0;
 		let gpa = 0;
 		let qrcode = ` Roll No : ${data.rollno} \n Name :  ${data.studentName}  \n Programme : ${data.program} \n Semester : ${data.semester}  \n Session: ${data.session}  \n Exam Type : ${data.papertype}  `;
@@ -83,16 +314,13 @@ console.log("Result",this.props)
 				return 'Fail';
 			}
 		}
-		if (!this.state.result) {
-			return <div>Loading....</div>;
-		}
-		return (
-			<div className={styles.container}>
-			<div style={{ position: 'relative' }}>
+    return (
+        <div className={styles.container}>
+		<div style={{ position: 'relative' }}>
 				<div style={{ position: 'absolute', top: '50%', left: '90%' }}>
 					<h1>Absolute</h1>
 				</div>
-				{/* Term back formula and grading formula */}
+				
 				<div style={{ display: 'flex' }}>
 					<div style={{ border: '1px solid black', marginRight: '70px' }}>
 						<p
@@ -157,26 +385,25 @@ console.log("Result",this.props)
 							fontSize: '30px'
 						}}
 					>
-						{/* {data.ptype} Studies ({data.department}) {result[0].studentId.program} ( */}
-					{/* {data.pduration} Years) */}
+				
 				</h1>
-					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					{/* <div style={{ display: 'flex', justifyContent: 'space-between' }}>
 						<p>Department of {data.department}</p>
 						<p>Session {data.session}</p>
-					</div>
-					{/* Name  */}
-					<p>
+					</div> */}
+					
+					{/* <p>
 						{data.gender === 0 ? <span>Mr.</span> : <span>Miss.</span>}
-						{this.state.result.length > 0 && this.state.result[0].studentId.firstName}{' '}
-						{this.state.result.length > 0 && this.state.result[0].studentId.lastName}
-					</p>
+						{result  && result[0].studentId.firstName}
+						{result && result[0].studentId.lastName}
+					</p> */}
 				</div>
 				<table>
 					<tbody>
 						<tr>
-							<td>
+							{/* <td>
 								{data.semester} semester {data.session} {data.papertype}
-							</td>
+							</td> */}
 							<td>Credit hours</td>
 							<td>Obt</td>
 							<td>Grade</td>
@@ -193,7 +420,7 @@ console.log("Result",this.props)
 							})}
 							<td />
 						</tr> */}
-						{this.state.result.map((sb) => {
+						{/* {result.map((sb) => {
 							return (
 								<tr> 
 									 <td>{sb.subjectId.name}</td>
@@ -214,7 +441,7 @@ console.log("Result",this.props)
 									</td> 
 								 </tr>
 							);
-						})}
+						})} */}
 						<tr> 
 							<td><b>GPA</b></td> <td>{(gpa = gp / totSub).toFixed(2)}</td>
 							<td style={{ display: 'none' }}>{(qrcode += `\n GPA : ${gpa.toFixed(2)} `)}</td>
@@ -237,8 +464,7 @@ console.log("Result",this.props)
 					</tbody>
 				</table>
 				<QRCode value={qrcode} />
+			</div> 
 			</div>
-			</div>
-		);
-	};
+    )
 }
--- frontend/src/views/report/printComponent.js
+++ frontend/src/views/report/printComponent.js
@@ -1,7 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef,useState,useEffect } from 'react';
 import ReactToPrint from 'react-to-print';
-import {ComponentToPrint} from './data';
+import ComponentToPrint from './data';
 function PrintComponent() {
+    const [result,setResult]=useState()
+    useEffect(() => {
+        fetch('http://localhost:4000/marks/marksStudent/6062c2a05cda4a2e94ce2452')
+			.then((js) => js.json())
+            .then((res) => {
+                setResult(res)
+			// this.setState({result:res})
+			 console.log("result",res)
+		});
+    }, [])
 	const componentRef = useRef();
 	// const handlePrint = useReactToPrint({ content: () => componentRef.current });
 	return (
@@ -9,7 +19,7 @@ function PrintComponent() {
             <ReactToPrint trigger={() => <button>Print This Out</button>}
             content={()=>componentRef.current}
             />
-			<ComponentToPrint ref={componentRef} />
+			<ComponentToPrint name="mohsin" ref={componentRef} />
 			
 		</div>
 	);
