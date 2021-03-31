911f5f00efbec9a56f8796f94950f31dc54ea9e3
tabarak5 <tacitim5@gmail.com>
2021-03-31 09:44:54 +0500

    Update Header.js

--- frontend/src/components/Headers/Header.js
+++ frontend/src/components/Headers/Header.js
@@ -42,50 +42,8 @@ const Header = () => {
                   </CardBody>
                 </Card>
               </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Admin
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">5</span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
-                        <i className="fas fa-users" />
-                        </div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                         Reports Generated
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">500</span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
-                        </div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
+        
+           
              </Row>
           </div>
         </Container>
